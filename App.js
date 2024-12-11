f794b31a1941b154b57b6595c8ab3ccb21cc9c1d
HARSH KUMAR GUPTA <133455584+hars728@users.noreply.github.com>
2024-12-11 13:12:20 +0530

    first change

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const jsxHeading = <h1>Nameste React Cool</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(jsxHeading);
+root.render(jsxHeading);
+
