2daaa260f340f2bd2357a0f7dc3c5cd87c10a311
HARSH KUMAR GUPTA <133455584+hars728@users.noreply.github.com>
2024-12-12 13:14:00 +0530

    making headers

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = <h1>Nameste React Cool</h1>
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://imgs.search.brave.com/jtJLvj-gHuaHg-6hRBY-s8yTVBWImiri3Vay4eytjVw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/c2hvcGlmeS5jb20v/cy9maWxlcy8xLzAx/OTQvNTU2My85NjUy/L3QvMTIvYXNzZXRz/L2VYUkdVWVR5Ulkt/YWxwaGEtbW90by1s/b2dvLXBuZy5wbmc_/dj0xNTk1Nzg5NzYy" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contect Us</li>
+                    <li>login</li>
+                </ul>
+
+            </div>
+        </div>
+    );
+};
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(jsxHeading);
-root.render(jsxHeading);
+
+root.render(<AppLayout />);
 
